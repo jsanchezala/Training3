6b7c10f6472d4a2df5301053a2541167771d7fbb
JJrose92 <juan-sa92@hotmail.com>
2018-10-31 01:03:31 +0100

    "Corregido nombre practica"

--- src/main/java/springBoot/mvc/practica2/Practica3Client.java
+++ src/main/java/springBoot/mvc/practica2/Practica3Client.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Practica2Application {
+public class Practica3Client {
     public static void main(String[] args) {
-        SpringApplication.run(Practica2Application.class, args);
+        SpringApplication.run(Practica3Client.class, args);
     }
 }
